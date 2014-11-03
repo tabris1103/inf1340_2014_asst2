989f02d64a2a049907ce1eed78b1b3e2cec0762e
Kyungho Jung <kyungho.jung@mail.utoronto.ca>
2014-11-03 10:25:17 -0500

    papers.py with program design plan + place holders for the required functions for the entire program Jordan, please work with
    is_visa_required
    is_in_watchlist
    parse_json_to_python_list
    valid_visa_code_format
    
    functions.

--- papers.py
+++ papers.py
@@ -13,6 +13,7 @@ __status__ = "Prototype"
 # imports one per line
 import re
 import datetime
+from datetime import date
 import json
 
 
@@ -26,7 +27,34 @@ def decide(input_file, watchlist_file, countries_file):
         an entry or transit visa is required, and whether there is currently a medical advisory
     :return: List of strings. Possible values of strings are: "Accept", "Reject", "Secondary", and "Quarantine"
     """
-    return ["Reject"]
+
+    entry_decision_list = []
+    #Check the existence of the file
+
+
+    # needs to have for loop to iterate the list
+        #For each entry information, store them into the corresponding variables
+
+
+        # via info variables - set the via information availability to false
+        # if there's no via key in the entry record
+
+
+        # Visa info variables - set the Viaa information availability to false
+        # if there's no Visa key in the entry record
+
+        # Check the input format validity for each field in the entry record
+
+        # Check whether the current entry record is in the watchlist, then decide to accept or send to secondary
+
+        # Check whether the current entry record is coming from or via country that has medical advisory
+        # then decide to accept or send to quarantine
+
+        # Check whether visa is required. If required, check whether entry record has correct visa information
+
+        # Compile each decision, then make final decision by the order of priority
+
+    return entry_decision_list
 
 
 def valid_passport_format(passport_number):
@@ -37,7 +65,8 @@ def valid_passport_format(passport_number):
     """
     passport_format = re.compile('.{5}-.{5}-.{5}-.{5}-.{5}')
 
-    if passport_format.match(passport_number):
+    if passport_format.match(passport_number) and re.match('^[a-zA-Z0-9-]+$', passport_number) is not None\
+            and len(passport_number.replace('-', '')) == 25:
         return True
     else:
         return False
@@ -54,3 +83,80 @@ def valid_date_format(date_string):
         return True
     except ValueError:
         return False
+
+
+def valid_visa_code_format(visa_code):
+    """
+    Checks whether the given Visa code has a valid format
+    :param visa_code: Visa code to be checked
+    :return: Boolean True if Visa format is valid, False otherwise
+    """
+
+def check_visa_expiry(visa_date):
+    """
+    Checks whether the given Visa is expired or not (Visa must be less than 2 years old)
+    :param visa_date: Visa date to be checked
+    :return: Boolean True if Visa date is less than 2 years old, False otherwise
+    """
+
+    # Get today's date, then subtract 2 years from it using try/except clause
+    # to consider leap year.
+    today_date = datetime.date.today()
+    try:
+        two_years_ago_from_today = today_date.replace(year=today_date.year - 2)
+    except ValueError:
+        two_years_ago_from_today = today_date + (date(today_date.year - 2, 1, 1) - date(today_date.year, 1, 1))
+
+    visa_date = datetime.datetime.strptime(visa_date, '%Y-%m-%d')
+
+    time_difference_in_days = (visa_date.date() - two_years_ago_from_today).days
+    if time_difference_in_days > 0:
+        return True
+    else:
+        return False
+
+def has_medical_advisory(country_code, country_info_list):
+    """
+    Check whether the given country (country code) has a medical advisory alert
+    :param country_code: Country Code - 3 letters
+    :param country_info_list: List that contains medical advisory information for each country
+    :return: Boolean True if country is under medical advisory, False otherwise.
+    """
+
+
+
+def parse_json_to_python_list (json_file):
+    """
+    Parse JSON file to python list and return the parsed list.
+    :param json_file: JSON file to be parsed
+    :return:List, List file generated from parsed JSON file
+    """
+    return parsed_list
+
+def valid_basic_info_availability(entry_record):
+    """
+    Check whether the person has all the required basic information available.
+    :param entry_record: entry record to be checked for the basic information availability
+    :return:True if all the information exists, False otherwise.
+    """
+
+    return False
+
+def is_visa_required(country_code, required_visa_type, country_list):
+    """
+    Check whether the country requires the transit/visitor visa or not
+    :param country_code: Country Code - 3 Characters
+    :param required_visa_type: Required Visa Type - transit/visit
+    :param country_list: list of country with Visa requirement information
+    :return: True if transit visa is required. False otherwise
+    """
+
+def is_in_watchlist(first_name, last_name, passport_number, watchlist):
+    """
+    Check whether the provided personal information is in the provided watchlist
+    :param first_name: first name to be checked
+    :param last_name:  last name to be checked
+    :param passport_number: passport number to be checked
+    :param watchlist: watchlist to be checked against
+    :return: True if match was found in the watchlist, False otherwise
+    """
