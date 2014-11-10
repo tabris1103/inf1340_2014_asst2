eb9af9bc85c7860467ea4807a0bebf890a10b435
Kyungho Jung <kyungho.jung@mail.utoronto.ca>
2014-11-10 02:44:38 -0500

    1. Added valid_basic_info_availability function 2. Fixed the basic information validity structure + variable assigning stage 3. Corrected the visa code and date assignment to the variables 4. added several printing functions for debugging 5. removed unnecessary boolean variables

--- papers.py
+++ papers.py
@@ -39,51 +39,50 @@ def decide(input_file, watchlist_file, countries_file):
     for entry_record in entry_list:
         entry_decision_for_current_record = []
 
-        #For each entry information, store them into the corresponding variables
-        first_name = entry_record['first_name']
-        last_name = entry_record['last_name']
-        birth_date = entry_record['birth_date']
-        passport = entry_record['passport']
-        home_city = entry_record['home']['city']
-        home_region = entry_record['home']['region']
-        home_country = entry_record['home']['country']
-        from_city = entry_record['from']['city']
-        from_region = entry_record['from']['region']
-        from_country = entry_record['from']['country']
-        entry_reason = entry_record['entry_reason']
-
-        # via info variables - set the via information availability to false
-        # if there's no via key in the entry record
-        via_info_availability = True
+        #For each entry information, store them into the corresponding variables if the variable exists.
         try:
-            via_city = entry_record['via']['city']
-            via_region = entry_record['via']['region']
-            via_country = entry_record['via']['country']
-        except KeyError:
-            via_info_availability = False
+            first_name = valid_basic_info_availability(entry_record['first_name'])
+            last_name = valid_basic_info_availability(entry_record['last_name'])
+            birth_date = valid_basic_info_availability(entry_record['birth_date'])
+            passport = valid_basic_info_availability(entry_record['passport'])
+            home_city = valid_basic_info_availability(entry_record['home']['city'])
+            home_region = valid_basic_info_availability(entry_record['home']['region'])
+            home_country = valid_basic_info_availability(entry_record['home']['country'])
+            from_city = valid_basic_info_availability(entry_record['from']['city'])
+            from_region = valid_basic_info_availability(entry_record['from']['region'])
+            from_country = valid_basic_info_availability(entry_record['from']['country'])
+            entry_reason = valid_basic_info_availability(entry_record['entry_reason'])
 
-        # Visa info variables - set the Visa information availability to false
-        # if there's no Visa key in the entry record
-        visa_info_availability = True
-        try:
-            visa_date = entry_record['visa']['code']
-            visa_code = entry_record['visa']['date']
-        except KeyError:
-            visa_info_availability = False
+            # store via variable only when via key is in the record
+            if 'via' in entry_record:
+                via_city = valid_basic_info_availability(entry_record['via']['city'])
+                via_region = valid_basic_info_availability(entry_record['via']['region'])
+                via_country = valid_basic_info_availability(entry_record['via']['country'])
+            # store visa variable only when visa key is in the record
+            if 'visa' in entry_record:
+                visa_date = valid_basic_info_availability(entry_record['visa']['date'])
+                visa_code = valid_basic_info_availability(entry_record['visa']['code'])
+        except ValueError:
+            basic_info_availability_decision = 'Reject'
+        else:
+            basic_info_availability_decision = 'Accept'
 
-        # Check the date input format validity for each date field in the entry record
+        #-- Check the date input format validity for each date field in the entry record
         if valid_passport_format(passport) is False:
             information_validity_decision = 'Reject'
         elif valid_date_format(birth_date) is False:
             information_validity_decision = 'Reject'
-        elif visa_info_availability is True and valid_date_format(visa_date) is False:
-            information_validity_decision = 'Reject'
-        elif visa_info_availability is True and valid_visa_code_format(visa_code) is False:
-            information_validity_decision = 'Reject'
+        elif 'visa' in entry_record:
+            if valid_date_format(visa_date) is False:
+                information_validity_decision = 'Reject'
+            elif valid_visa_code_format(visa_code) is False:
+                information_validity_decision = 'Reject'
+            else:
+                information_validity_decision = 'Accept'
         else:
             information_validity_decision = 'Accept'
 
-        # Check whether the current entry record is in the watchlist, then decide to accept or send to secondary
+        #-- Check whether the current entry record is in the watchlist, then decide to accept or send to secondary
         if is_in_watchlist(first_name, last_name, passport, watch_list):
             watchlist_decision = 'Secondary'
         else:
@@ -91,7 +90,7 @@ def decide(input_file, watchlist_file, countries_file):
 
         # Check whether the current entry record is coming from or via country that has medical advisory
         # then decide to accept or send to quarantine
-        if via_info_availability:
+        if 'via' in entry_record:
             check_medical_advisory = has_medical_advisory(via_country, country_list) or \
                 has_medical_advisory(from_country, country_list)
         else:
@@ -101,7 +100,7 @@ def decide(input_file, watchlist_file, countries_file):
 
         # Check whether visa is required. If required, check whether entry record has correct visa information
         if is_visa_required(home_country, entry_reason, country_list):
-            if visa_info_availability:
+            if 'visa' in entry_record:
                 if valid_date_format(visa_date) and valid_visa_code_format(visa_code) and check_visa_expiry(visa_date):
                     visa_decision = 'Accept'
                 else:
@@ -111,17 +110,26 @@ def decide(input_file, watchlist_file, countries_file):
         else:
             visa_decision = 'Accept'
 
-        if entry_reason == 'returning' and home_country == 'KAN':
+        if entry_reason.lower() == 'returning' and home_country.upper() == 'KAN':
             return_entry_decision = 'Accept'
         else:
-            return_entry_decision = ''
+            return_entry_decision = 'N/A'
 
         # Compile each decision, then make final decision by the order of priority
+        entry_decision_for_current_record.append(basic_info_availability_decision)
         entry_decision_for_current_record.append(return_entry_decision)
         entry_decision_for_current_record.append(watchlist_decision)
         entry_decision_for_current_record.append(visa_decision)
         entry_decision_for_current_record.append(medical_advisory_decision)
         entry_decision_for_current_record.append(information_validity_decision)
+
+        print('info_complete_decision: ' + basic_info_availability_decision)
+        print('return_entry_decision: ' + return_entry_decision)
+        print('watchlist_decision: ' + watchlist_decision)
+        print('visa_decision: ' + visa_decision)
+        print('medical_advisory_decision: ' + medical_advisory_decision)
+        print('info_validity_decision: ' + information_validity_decision)
+        print(' ')
 
         if 'Quarantine' in entry_decision_for_current_record:
             entry_decision_list.append('Quarantine')
@@ -135,9 +143,10 @@ def decide(input_file, watchlist_file, countries_file):
     return entry_decision_list
 
 
+
 def valid_passport_format(passport_number):
     """
-    Checks whether a pasport number is five sets of five alpha-number characters separated by dashes
+    Checks whether a passports number is five sets of five alpha-number characters separated by dashes
     :param passport_number: alpha-numeric string
     :return: Boolean; True if the format is valid, False otherwise
     """
@@ -220,17 +229,21 @@ def parse_json_to_python_list (json_file):
     json_file_reader = open(json_file, "r")
     file_contents = json_file_reader.read()
     parsed_list = json.loads(file_contents)
+    json_file_reader.close()
 
     return parsed_list
 
-def valid_basic_info_availability(entry_record):
+
+def valid_basic_info_availability(key_value):
     """
     Check whether the person has all the required basic information available.
     :param entry_record: entry record to be checked for the basic information availability
     :return:True if all the information exists, False otherwise.
     """
-
-    return False
+    if key_value == "":
+        raise ValueError("Field value is not complete!")
+    else:
+        return key_value
 
 def is_visa_required(country_code, required_visa_type, country_list):
     """
@@ -242,7 +255,7 @@ def is_visa_required(country_code, required_visa_type, country_list):
     """
     if country_code == 'KAN':
         return False
-    if required_visa_type == 'transit':
+    if required_visa_type.lower() == 'transit':
         if country_list[country_code]['transit_visa_required'] == '1':
             return True
         else:
